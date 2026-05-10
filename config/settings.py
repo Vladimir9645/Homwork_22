424c3039d77ef3838e17b94aa160109462f6c753
Vladimir9645 <rudoruka@gmail.com>
2026-05-10 08:18:33 +0300

    New

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#7oqc#&eq*59q!lyg3tq0cdr)9*xc)#od-l=1exr!kquueqzwn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog'
 ]
 
 MIDDLEWARE = [
